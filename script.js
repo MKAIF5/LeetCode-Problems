06404e08b3cf3992b01267bc27f336550d37a060
mohammed kaif <mkaif555123@gmail.com>
2024-06-09 15:51:29 +0500

    loop practice

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // console.log("JS LOGIC BUILDING");
 
-
 //Array Parctices
+
 // console.log([4] + [5] + [6]);
 
-
-
-
-
 //Loop practices
+
 // for(i=1; i<=5; i++){
 //     for(j=1; j<=3; j++){
 //         console.log(i);
 //     }
 // }
+
+let i = 1
+while (i <= 5) {
+    i++
+}
+console.log(i);
