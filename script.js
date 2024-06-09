40bc7bb1fe6dbcc63e3bf661d3a3abe0f109223d
mohammed kaif <mkaif555123@gmail.com>
2024-06-09 15:46:52 +0500

    loop and array practices start

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // console.log("JS LOGIC BUILDING");
 
+
+//Array Parctices
 // console.log([4] + [5] + [6]);
+
+
+
+
+
+//Loop practices
+// for(i=1; i<=5; i++){
+//     for(j=1; j<=3; j++){
+//         console.log(i);
+//     }
+// }
