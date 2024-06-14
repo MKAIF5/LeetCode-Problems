8839975a19aed7cbb7c0501c1da4ba4900a4188b
mohammed kaif <mkaif555123@gmail.com>
2024-06-14 13:47:33 +0500

    problem 3 solved

--- JS-LEETCODE SOLVED/script.js
+++ JS-LEETCODE SOLVED/script.js
@@ -1,10 +1,26 @@
-// solved 1 : Create Hello World Function 
+// Solved 1 : Create Hello World Function 
 
-const createHelloWorld = function() {
+// const createHelloWorld = function() {
     
-    return function(...args) {
-      return "hello world"
-    }
-};
+//     return function(...args) {
+//       return "hello world"
+//     }
+// };
 
-createHelloWorld()
+// createHelloWorld();
+
+//Solved 2 : Counter
+
+// const createCounter = function(n) {
+    
+//     return function() {
+        
+//     };
+// };
+
+//Solved 3 : Palindrome Number
+
+// const isPalindrome = function(x) {
+//    let reversed = x.toString().split("").reverse().join("")  
+//    return x.toString() === reversed
+// };
