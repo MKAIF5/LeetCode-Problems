6178d2f1efc0fe7cca000a6c3c7b91798156af7b
mohammed kaif <mkaif555123@gmail.com>
2024-08-02 16:12:01 +0500

    everything updated

--- JS-LEETCODE SOLVED/EASY/script.js
+++ JS-LEETCODE SOLVED/EASY/script.js
@@ -38,17 +38,33 @@ const romanToInt = function (s) {
     //     "D":500,
     //     "M":1000
     // }
-    let result  = 0;
+    let result = 0;
     for (i = 0; i < s.length; i++) {
         const curr = myMap[s[i]]
-        const next = myMap[s[i+ 1]]
+        const next = myMap[s[i + 1]]
     }
-    if(curr < next){
-       result += next - curr
-       i++
+    if (curr < next) {
+        result += next - curr
+        i++
     }
-    else{
-     result += curr
+    else {
+        result += curr
     }
     return result;
 };
+
+let details = ["7868190130M7522", "5303914400F9211", "9273338290F4010", "1313579440F2036", "2921522980M5644"];
+
+const seniorCounter = (details) => {
+    let count = 0;
+    for (let i = 0; i < details.length; i++) {
+        let age = details[i].replace(details[i], `${details[i].charAt(11)} ${details[i].charAt(12)}`);
+        let ageCheck = Number(age)
+        if (ageCheck >= 60) {
+            count++
+        }
+    }
+    return count
+}
+
+seniorCounter(details)
