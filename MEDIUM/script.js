c57c4447039f4f1e4099959794658bcc7e5244c6
mohammed kaif <mkaif555123@gmail.com>
2024-08-10 00:07:21 +0500

    Another activity

--- MEDIUM/script.js
+++ MEDIUM/script.js
@@ -173,3 +173,30 @@ const groupAnagrams = function (strs) {
 };
 
 console.log(groupAnagrams(["eat", "tab", "bat"]));
+
+// Integer to Roman
+
+const intToRoman = function (num) {
+
+  const M = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 9,
+    "IV": 4,
+    "I": 1
+  }
+  let result = "";
+  Object.entries(M).forEach(([k , v]) =>{
+    result += k.repeat(Math.floor(num / v));
+    num %= v
+  })
+  return result;
+};
