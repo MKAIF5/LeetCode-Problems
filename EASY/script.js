f61c29f1130b39770fc6c79bd5cc30915532d42d
mohammed kaif <mkaif555123@gmail.com>
2024-08-09 19:03:07 +0500

    another activity done

--- EASY/script.js
+++ EASY/script.js
@@ -73,10 +73,28 @@ seniorCounter(details)
 
 const removeElement = (nums, val) => {
     for (let i = nums.length - 1; i >= 0; i--) {
-      if (nums[i] === val) {
-        nums.splice(i, 1);
-      }
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+        }
     }
     return nums.length;
-  };
-  
+};
+
+
+// Solved : Longest Common Prefix
+
+const longestCommonPrefix = function (strs) {
+
+    if (strs.length === 0) {
+        return "";
+    }
+    
+    let prefix  = strs[0];
+    
+    for(let i = 1; i< strs.length; i++){
+        while(strs[i].indexOf(prefix) != 0){
+            prefix = prefix.substring(0, prefix.length - 1)
+        }
+    }
+    return prefix
+}
