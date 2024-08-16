f64bfcc4abd276b49373eca0d8fff35b6592df3e
mohammed kaif <mkaif555123@gmail.com>
2024-08-17 00:27:23 +0500

    reverse string completed

--- EASY/script.js
+++ EASY/script.js
@@ -196,3 +196,32 @@ const lengthOfLastWord = function (s) {
 };
 
 console.log(lengthOfLastWord("luffy is still joyboy"));
+
+
+
+// const reverseString = function (s) {
+//     return s.slice().reverse().join()
+// };
+
+// console.log(reverseString(["h" ,"e" , "l" , "l" , "o"]));
+
+const reverseString = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        // Swap the characters at the left and right pointers
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+};
+
+// Test cases
+let s1 = ["h", "e", "l", "l", "o"];
+reverseString(s1);
+console.log(s1); // Output: ["o", "l", "l", "e", "h"]
+
+let s2 = ["H", "a", "n", "n", "a", "h"];
+reverseString(s2);
+console.log(s2); // Output: ["h", "a", "n", "n", "a", "H"]
